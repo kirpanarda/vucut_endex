603477c4be0f53e46943e32b6e3a3ad87f19da0d
kirpanarda <kirpanarda@gmail.com>
2021-10-11 16:18:38 +0300

    arda kırpan
    boy kilo endeks

---
+++ BOY KİLO VÜCUT ENDEKSİ/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BOY_KİLO_VÜCUT_ENDEKSİ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double kg = Convert.ToDouble(textBox1.Text);
+            double m = Convert.ToDouble(textBox2.Text);
+            double vki = kg / (m * m);
+            if (vki <= 18.49)
+            {
+                MessageBox.Show("Ideal Kilonun Alti");
+            }
+            else if (vki >= 18.5 && vki <= 24.99)
+            {
+                MessageBox.Show("Ideal Kilo");
+            }
+            else if (vki >= 25 && vki <= 29.99)
+            {
+                MessageBox.Show("Ideal Kilonun Uzeri");
+            }
+            else 
+            {
+                MessageBox.Show("obez");
+            }
+
+
+        }
+    }
+}
